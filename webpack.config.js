d8dc1d83ab7de039f7617bb4552c365d846a407c
Max Crane <maxlccrane@gmail.com>
2017-07-14 23:39:06 -0700

    fix issue where background wouldn't completely cover background

--- webpack.config.js
+++ webpack.config.js
@@ -7,50 +7,53 @@ var DIST_DIR = path.resolve(__dirname, "dist");
 var SRC_DIR = path.resolve(__dirname, "src");
 
 var config = {
-	entry: SRC_DIR + "/app/index.jsx",
-	output: {
-		path : DIST_DIR + "/app",
-		filename : "bundle.js",
-		publicPath : "/app/"
-	},
-	module: {
-		loaders : [
-			{
-				test : /\.jsx?/,
-				include : SRC_DIR,
-				loader : "babel-loader",
-				query : {
-					presets : ["react", "es2015", "stage-2"]
-				} 
-			},
-			{
-				test : /\.css$/,
-				loader : "style-loader!css-loader"
-			}
-		]
-	},
-	devServer: {
-	    proxy: {
+    entry: SRC_DIR + "/app/index.jsx",
+    output: {
+        path: DIST_DIR + "/app",
+        filename: "bundle.js",
+        publicPath: "/app/"
+    },
+    module: {
+        loaders: [{
+                test: /\.jsx?/,
+                include: SRC_DIR,
+                loader: "babel-loader",
+                query: {
+                    presets: ["react", "es2015", "stage-2"]
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader?url=false"
+            }
+        ]
+    },
+    devServer: {
+        proxy: {
             '/api/**': {
                 target: 'http://localhost:3000/',
                 secure: false,
                 changeOrigin: true
             }
         }
-  	},
-  	context: SRC_DIR,
-  	plugins: [
-        new CopyWebpackPlugin([
-            { 
-              from: 'images',
-              to:  path.resolve(DIST_DIR, "images") 
-          	},
-          	{
-          		from: 'index.html',
-          		to: path.resolve(DIST_DIR, 'index.html')
-          	}
+    },
+    context: SRC_DIR,
+    plugins: [
+        new CopyWebpackPlugin([{
+                from: 'images',
+                to: path.resolve(DIST_DIR, "images")
+            },
+            {
+                from: 'index.html',
+                to: path.resolve(DIST_DIR, 'index.html')
+            }
         ])
-    ]
+    ],
+    resolve: {
+        alias: {
+            'images': path.resolve(SRC_DIR, "images")
+        }
+    }
 };
 
 module.exports = config;
